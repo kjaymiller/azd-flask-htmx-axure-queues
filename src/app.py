dc11f23456d6d81c0f9b659bbde95eb9aabb6d4e
Jay <kjaymiller@gmail.com>
2022-11-07 14:54:09 -0800

    moves to src and adds index

--- src/app.py
+++ src/app.py
@@ -1,16 +1,12 @@
 import logging
 from flask import Flask
 from azure.storage.queue import QueueClient
-from uuid import uuid4
-# import dotenv
 import os
-import pdb
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
-# dotenv.load_dotenv(".env")
 conn_str = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
 queue_name = os.environ.get("AZURE_STORAGE_QUEUE_NAME")
 
@@ -18,7 +14,6 @@ logging.info("Connecting to queue...")
 logging.debug(f"Connection string: {conn_str}")
 logging.debug(f"Queue name: {queue_name}")
 
-pdb.set_trace()
 queue = QueueClient.from_connection_string(
         conn_str=conn_str,
         queue_name=queue_name,
@@ -33,7 +28,6 @@ logging.info('Queue created')
 
 @app.route('/')
 def hello_world():                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
-    queue.send_message(str(uuid4()))
     content = queue.receive_message()
     print(content.content)
     queue.delete_message(content)
